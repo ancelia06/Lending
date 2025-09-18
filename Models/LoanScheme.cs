84cc61c707e0c9a720bd8e06ebd754dee0b2d713
Sidmagar <126499234+Sidmagar@users.noreply.github.com>
2025-09-18 21:10:37 +0530

    Add files via upload

---
+++ Models/LoanScheme.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lending_CapstoneProject.Models
+{
+    public class LoanScheme
+    {
+        [Key]
+        public int LoanSchemeId { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string SchemeName { get; set; }
+
+        [Required]
+        public decimal InterestRate { get; set; }
+
+        [Required]
+        public int MinCreditScore { get; set; }
+
+        public int CompanyId { get; set; }
+        public LoanBank LoanBank { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lending_CapstoneProject.Models
+{
+    public abstract class User
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string PasswordHash { get; set; }
+
+
+        [Required]
+        public UserType UserType { get; set; }
+
+        // Optional: each user may belong to a loan company
+        public int? LoanBankId { get; set; }
+        public LoanBank LoanBank{ get; set; }
+    }
+}
