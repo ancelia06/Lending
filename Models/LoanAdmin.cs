c419416b3a8669ce59a3390e20626f390dde7bd8
Sidmagar <126499234+Sidmagar@users.noreply.github.com>
2025-09-18 21:09:57 +0530

    Delete Models directory

--- Models/LoanAdmin.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Lending_CapstoneProject.Models
-{
-    public class LoanAdmin
-    {
-        public int? CompanyId { get; set; }
-        public LoanCompany LoanCompany { get; set; }
-    }
-}
--- Models/SystemAdministrator.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Lending_CapstoneProject.Models
-{
-    public class SystemAdministrator:User
-    {
-    public string Name { get; set; }
-    }
-}
-
