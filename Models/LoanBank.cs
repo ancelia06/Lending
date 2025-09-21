49e9b0229019c641cd1fff3cc6f3d2e5d3f7f459
Sidmagar <126499234+Sidmagar@users.noreply.github.com>
2025-09-21 17:03:44 +0530

    Add files via upload

---
+++ Models/LoanBank.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Lending_CapstoneProject.Models
+{
+    public class LoanBank
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] // <-- Add this line
+
+        public int BankId { get; set; }
+
+
+        [StringLength(255)]
+        public string BankName { get; set; }
+
+        [StringLength(255)]
+        public string Address { get; set; }
+
+        // Navigation properties
+        public ICollection<LoanScheme> LoanSchemes { get; set; }
+        public ICollection<LoanOfficer> LoanOfficers { get; set; }
+        public ICollection<LoanBranch> LoanBranches { get; set; }
+        // Change the f ile yo Bank
+        public ICollection<User> Users { get; set; }
+
+    }
+}
---
+++ Models/LoanOfficer.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lending_CapstoneProject.Models
+{
+    public class LoanOfficer : User
+    {
+        [StringLength(50)]
+        public string EmployeeId { get; set; }
+        public int BranchId { get; set; }
+        public LoanBranch Branch { get; set; }
+        public string AssignedCity { get; set; }
+
+
+        public ICollection<LoanApplication> AssignedApplications { get; set; }
+
+    }
+}
