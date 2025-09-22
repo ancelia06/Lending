59295dbbcecb98237af1a7a2d8039f9439c2d222
Sidmagar <126499234+Sidmagar@users.noreply.github.com>
2025-09-22 12:38:52 +0530

    Delete Services directory

--- Services/Interface/IEmailService.cs
+++
@@ -1,8 +0,0 @@
-﻿//namespace Lending_CapstoneProject.Services.Interface
-//{
-//    public interface IEmailService
-//    {
-//        Task SendEmailAsync(string toEmail, string subject, string message);
-
-//    }
-//}
